afea426bbe1c36dcc1d0b9dd78eb14a31abb2a4d
nor mohammed <44341598+mohammed-nor@users.noreply.github.com>
2025-03-08 00:45:05 +0000

    the package is no longer being depended on: google_generative_ai

--- test/chem_nor_test.dart
+++ test/chem_nor_test.dart
@@ -2,8 +2,7 @@ import 'package:chem_nor/chem_nor.dart';
 import 'package:test/test.dart';
 
 void main() async {
-  final finder =
-      ChemNOR(genAiApiKey: 'AIzaSyCR80a7Gb4kSGd5rX9ingZhJKSw9b9hQgQ');
+  final finder = ChemNOR(genAiApiKey: 'APIkey');
   dynamic properties = await finder.getCompoundProperties(248);
   print(properties);
   //print(finder.charge);
@@ -11,7 +10,6 @@ void main() async {
   print(list);
   final smiles = await finder.getRelevantSmiles('carboxylic acid compounds');
   print(smiles);
-  dynamic chat = await finder.chemist(
-      'hello , please educate me about carboxymethyl(trimethyl)ammonium ');
+  dynamic chat = await finder.chemist('hello , please educate me about carboxymethyl(trimethyl)ammonium ');
   print(chat);
 }
